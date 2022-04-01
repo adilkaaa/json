3e66a4b9d1dcbe97db9370a31e1d3499cb41b07f
adilkaaa <adilkhanakakov@gmail.com>
2022-04-01 12:22:45 +0600

    add json examples

---
+++ my_json.py
@@ -0,0 +1,66 @@
+#1 --------------------------------------------
+import json
+# dict_1 = {
+#     'login':'adilkaaa',
+#     'email':'adilkhan@gmail.com',
+#     'age':17,
+#     'phone_number':'0706200205'
+# }
+# dict_2 = { 
+#     'login':'beks',
+#     'email':'isakovbeksultan@gmail.com',
+#     'age':17,
+#     'phone_number':'0503111777'}
+# dict_3 = {
+#     'login':'aziret',
+#     'email':'lifeix@gmail.com',
+#     'age':15,
+#     'phone_number':'0704260100'
+# }
+
+# all_dicts = [dict_1,dict_2,dict_3]
+
+# #write 3 dicts to json file
+# with open('dicts.json', 'w') as f:
+#     json.dump(all_dicts,f)
+
+# #load json file
+# with open('dicts.json', 'r') as f:
+#     j = json.load(f)
+
+# #add 'language' key to every dict
+# for i in range(3):
+#     j[i]['language'] = 'english'
+
+# #update json file
+# with open('dicts.json','w') as f:
+    # json.dump(j,f)
+
+
+# 2-----------------------------------------------
+# from pprint import pprint as pp
+# #loading json file from github
+# with open('events.json', 'r') as f:
+#     j = json.load(f)
+
+# #adding 'actor' key to every element
+# for i in range(len(j)):
+#     j[i]['actor'] = 'Kayrat Sexy'
+
+# #writing to new json file
+# with open('ex2.json','w') as f:
+#     json.dump(j,f)
+
+# #checking
+# with open('ex2.json','r') as f:
+#     my_j = json.load(f)
+
+# for i in range(len(my_j)):
+#     print(my_j[i]['actor'])
+
+
+
+
+
+
+
